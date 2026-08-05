4c03a60f13c1ab595e2d060605a3b5c5acf6590f
rojim666 <771473941@qq.com>
2026-08-05 21:12:15 +0800

    chore(desktop): remove generated cache files

--- desktop/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-export default defineConfig({
-    plugins: [vue()],
-    clearScreen: false,
-    server: { port: 5173, strictPort: true },
-    envPrefix: ["VITE_", "TAURI_"],
-    build: { target: "es2022", minify: !process.env.TAURI_DEBUG },
-});
